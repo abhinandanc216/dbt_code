a97ae165abdc197c3dd3fbaeca9b84c0aed05c24
abhinandan.c3057@gmail.com <abhinandan.c3057@gmail.com>
2025-02-14 17:43:08 +0000

    added macro

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -19,9 +19,12 @@ c.segment,
 c.country,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 LEFT JOIN {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
+{{limit_data_in_dev('o.orderdate')}}
