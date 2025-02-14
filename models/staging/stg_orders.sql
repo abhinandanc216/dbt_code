fc23f10f54f9402d0203d8cd64b55498d44be64f
abhinandan.c3057@gmail.com <abhinandan.c3057@gmail.com>
2025-02-14 18:05:17 +0000

    added delivery_team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -21,11 +21,12 @@ c.country,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice', 'ordercostprice') }} as markup
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 LEFT JOIN {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_product') }} as p
 on o.productid = p.productid
-
-{{limit_data_in_dev('o.orderdate')}}
+LEFT JOIN {{ ref('delivery_team') }} as d
+ON o.shipmode = d.shipmode
